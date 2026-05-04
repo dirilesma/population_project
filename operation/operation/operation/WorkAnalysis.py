cb0eb9b9a57c7cb381bc9217429d18e9c44983ae
dirilesma <dirilesma25@gmail.com>
2026-05-04 18:14:22 +0300

    Update WorkAnalysis.py

--- operation/operation/operation/WorkAnalysis.py
+++ operation/operation/operation/WorkAnalysis.py
@@ -1,1 +1,10 @@
+class WorkAnalysis:
+    def __init__(self, data):
+        self.data = data
+        self.toplam = len(data)
 
+    def working_percentage(self):
+        if self.toplam == 0:
+            return 0.0
+        calisan = sum(1 for k in self.data if k.is_working == 1)
+        return round((calisan / self.toplam) * 100, 2)
