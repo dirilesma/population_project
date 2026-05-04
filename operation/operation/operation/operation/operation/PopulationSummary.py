9a54251a83b30022b01e3bc8792b114c0489d57c
dirilesma <dirilesma25@gmail.com>
2026-05-04 18:15:10 +0300

    Update PopulationSummary.py

--- operation/operation/operation/operation/operation/PopulationSummary.py
+++ operation/operation/operation/operation/operation/PopulationSummary.py
@@ -1,1 +1,9 @@
+class PopulationSummary:
+    def __init__(self, data):
+        self.data = data
 
+    def total_population(self):
+        return len(self.data)
+
+    def summary(self):
+        return {"Toplam Kişi": self.total_population()}
