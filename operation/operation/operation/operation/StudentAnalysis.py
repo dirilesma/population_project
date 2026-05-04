5d49e8dcb910c0d1040664b722057a5f09746918
dirilesma <dirilesma25@gmail.com>
2026-05-04 18:14:45 +0300

    Update StudentAnalysis.py

--- operation/operation/operation/operation/StudentAnalysis.py
+++ operation/operation/operation/operation/StudentAnalysis.py
@@ -1,1 +1,10 @@
+class StudentAnalysis:
+    def __init__(self, data):
+        self.data = data
+        self.toplam = len(data)
 
+    def student_percentage(self):
+        if self.toplam == 0:
+            return 0.0
+        ogrenci = sum(1 for k in self.data if k.is_student == 1)
+        return round((ogrenci / self.toplam) * 100, 2)
